18dde57280a0b8b495487829fd7f592398eeda37
karthii123-wq <tiruveedhulakarthik@gmail.com>
2026-04-27 15:26:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const topics = document.querySelectorAll(".topic");
+
+topics.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const content = btn.nextElementSibling;
+
+    document.querySelectorAll(".content").forEach(c => {
+      if (c !== content) c.style.display = "none";
+    });
+
+    content.style.display =
+      content.style.display === "block" ? "none" : "block";
+  });
+});
